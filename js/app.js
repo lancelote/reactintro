a94947aa216ddfd45dc951d37dcc7c6f67cf6afa
Pavel Karateev <karateev.pavel@ya.ru>
2015-12-25 21:18:31 +0300

    React composition

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+// Array of available channels
+let channels = [
+    {name: "Hardware Support"},
+    {name: "Software Support"}
+];
+
+// Single channel
 class Channel extends React.Component {
     onClick() {
         console.log("I was clicked", this.props.name);
@@ -10,7 +17,25 @@ class Channel extends React.Component {
     }
 }
 
+// List of Channels
+class ChannelList extends React.Component {
+    render() {
+        return (
+            <ul>
+                {
+                    this.props.channels.map(channel => {
+                        return (
+                            <Channel name={channel.name} />
+                        )
+                    })
+                }
+            </ul>
+        )
+    }
+}
+
+// Display content
 ReactDOM.render(
-    <Channel name="Hardware Support"/>,
+    <ChannelList channels={channels}/>,
     document.getElementById('app')
 );
